24e13e7c164c465ccf5f01b8120d383eddbfc15e
Sadhika-dotcom <s4872328@student.uq.edu.au>
2024-10-03 05:25:42 +0530

    Html pages and console
    Added other html pages, and console for javascript

---
+++ assessment/javascript/general_modules.js
@@ -0,0 +1,5 @@
+function load() {
+    console.log("Javascript is being loaded!");
+}
+
+export {load};
--- assessment/javascript/script.js
+++ assessment/javascript/script.js
@@ -1,29 +1,5 @@
-// Function to handle carousel swipe or scroll
-const carousel = document.querySelector('.carousel');
+import { load } from "./general_modules.js";
 
-// Adding swipe functionality for mobile
-let isDown = false;
-let startX;
-let scrollLeft;
 
-carousel.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX - carousel.offsetLeft;
-    scrollLeft = carousel.scrollLeft;
-});
 
-carousel.addEventListener('mouseleave', () => {
-    isDown = false;
-});
-
-carousel.addEventListener('mouseup', () => {
-    isDown = false;
-});
-
-carousel.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - carousel.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    carousel.scrollLeft = scrollLeft - walk;
-});
+load();
