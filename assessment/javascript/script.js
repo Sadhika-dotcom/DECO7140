fc7c5e0dcaae2c7b76c641cae99e55b3c391206e
Sadhika-dotcom <s4872328@student.uq.edu.au>
2024-11-03 20:33:42 +1000

    Added carousel fuctionality and recommended games
    Stylized the website a bit more and added fully functional carousel which shows different games when clicked.

--- assessment/javascript/script.js
+++ assessment/javascript/script.js
@@ -1,5 +1,18 @@
-import { load } from "./components.js";
+//Import functionality from other modules
+
+//Define variables
+
+//Attach event listeners to elements
+
+//Startup code that runs at or during page load
+alert("JavaScript works!");
+console. log ("script loaded");
+
+//Functions to define specific behaviours
+
+//Functions for general use
 
 
 
-load();
+
+
